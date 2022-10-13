9e8ca92bf1d42a4eee5529d013246aba10eca51e
mevlut caymaz <mevlitkacmaz.63@gmail.com>
2022-10-13 23:22:54 +0300

    13.10.2022

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,9 +4,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
+        plugin={"html:target\\cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml" },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags =   " @ah", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
+        tags = "@all", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
         dryRun = false
 )
 public class Runner {
